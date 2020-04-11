4f72a848fcf7bf776dd6d3a3fd8e38e6e2b14977
imsikun <imsikun1@gmail.com>
2020-04-11 14:29:21 +0530

    typo error

--- src/components/Chart/Charts.jsx
+++ src/components/Chart/Charts.jsx
@@ -59,7 +59,7 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
       }}
       options={{
         legend: { display: false },
-        title: { display: true, text: `Current State in ${country}` },
+        title: { display: true, text: `Current Status in ${country}` },
       }}
     />
   ) : null
